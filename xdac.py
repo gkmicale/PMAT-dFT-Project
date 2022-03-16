537639b26d6927b304b694356b53c4537d2f4eac
Gillian Micale <gkmicale@mit.edu>
2022-03-16 12:11:05 -0400

    Changed default key in unlock function to reflect PMAT's unit

--- xdac.py
+++ xdac.py
@@ -38,7 +38,7 @@ class XDAC:
             3: (0, 40)
         }
 
-    def unlock(self, key='nicslab'):
+    def unlock(self, key='169.254.57.13'):
         # Send Request to XDAC (Server)
         msg = "GETINFO:" + key
         self.req_socket.send(msg.encode('utf-8'))
