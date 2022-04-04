5fee5a6bde43b172b997812eefd533178539aa2d
Gillian Micale <gkmicale@mit.edu>
2022-04-04 19:10:19 -0400

    <ade corrections to maps to account for south PCB design flaw (1-64 flipped on circuit design). Will only use north pcb connector.

--- connector_map.py
+++ connector_map.py
@@ -19,77 +19,80 @@ class AdapterBoard:
     north = chip_pin_out.highres_pin_map
 
     eq_connector_map = {
-        south[2]:  J7[1 - 1],  # J7, 1
-        south[3]:  J7[2 - 1],  # J7, 2
-        south[4]:  J7[3 - 1],  # J7, 3
-        south[5]:  J7[4 - 1],  # J7, 4
-        south[6]:  J7[5 - 1],  # J7, 5
-        south[7]:  J7[6 - 1],  # J7, 6
-        south[8]:  J7[7 - 1],  # J7, 7
-        south[9]:  J7[8 - 1],  # J7, 8
-        south[10]: J7[9 - 1],  # J7, 9
-        south[11]: J7[10 - 1], # J7, 10
-        south[12]: J7[11 - 1], # J7, 11
-        south[13]: J7[12 - 1], # J7, 12
-        south[14]: J7[13 - 1], # J7, 13
-        south[15]: J7[14 - 1], # J7, 14
-        south[16]: J7[15 - 1], # J7, 15
-        south[17]: J7[16 - 1], # J7, 16
-        south[18]: J7[17 - 1], # J7, 17
-        south[19]: J7[18 - 1], # J7, 18
-        south[20]: J7[19 - 1], # J7, 19
-        south[21]: J7[20 - 1], # J7, 20
-        south[22]: J8[1 - 1],  # J8, 1
-        south[23]: J8[2 - 1],  # J8, 2
-        south[24]: J8[3 - 1],  # J8, 3
-        south[25]: J8[4 - 1],  # J8, 4
-        south[26]: J8[5 - 1],  # J8, 5
-        south[27]: J8[6 - 1],  # J8, 6
+        south[1]: J4[20 - 1],  # J4, 20
+        south[2]: J4[19 - 1],  # J4, 19
+        south[3]: J4[18 - 1],  # J4, 18
+        south[4]: J4[17 - 1],  # J4, 17
+        south[5]: J4[16 - 1],  # J4, 16
+        south[6]: J4[15 - 1],  # J4, 15
+        south[7]: J4[14 - 1],  # J4, 14
+        south[8]: J4[13 - 1],  # J4, 13
+        south[9]: J4[12 - 1],  # J4, 12
+        south[10]: J4[11 - 1],  # J4, 11
+        south[11]: J4[10 - 1],  # J4, 10
+        south[12]: J4[9 - 1],  # J4, 9
+        south[13]: J4[8 - 1],  # J4, 8
+        south[14]: J4[7 - 1],  # J4, 7
+        south[15]: J4[6 - 1],  # J4, 6
+        south[16]: J4[5 - 1],  # J4, 5
+        south[17]: J4[4 - 1],  # J4, 4
+        south[18]: J4[3 - 1],  # J4, 3
+        south[19]: J4[2 - 1],  # J4, 2
+        south[20]: J4[1 - 1],  # J4, 1
+        south[21]: J3[20 - 1],  # J3, 20
+        south[22]: J3[19 - 1],  # J3, 19
+        south[23]: J3[18 - 1],  # J3, 18
+        south[24]: J3[17 - 1],  # J3, 17
+        south[25]: J3[16 - 1],  # J3, 16
+        south[26]: J3[15 - 1],  # J3, 15
+        south[27]: J3[14 - 1],  # J3, 14
     }
 
     highres_connector_map = {
-        north[28]: J3[4 - 1],   # J3, 4
-        north[29]: J3[5 - 1],   # J3, 5
-        north[30]: J3[6 - 1],   # J3, 6
-        north[31]: J3[7 - 1],   # J3, 7
-        north[32]: J3[8 - 1],   # J3, 8
-        north[33]: J3[9 - 1],   # J3, 9
-        north[34]: J3[10 - 1],  # J3, 10
-        north[35]: J3[11 - 1],  # J3, 11
-        north[36]: J3[12 - 1],  # J3, 12
-        north[37]: J3[13 - 1],  # J3, 13
-        north[38]: J3[14 - 1],  # J3, 14
-        north[39]: J3[15 - 1],  # J3, 15
-        north[40]: J3[16 - 1],  # J3, 16
-        north[41]: J3[17 - 1],  # J3, 17
-        north[42]: J3[18 - 1],  # J3, 18
-        north[43]: J3[19 - 1],  # J3, 19
-        north[44]: J3[20 - 1],  # J3, 20
-        north[45]: J4[1 - 1],   # J4, 1
-        north[46]: J4[2 - 1],   # J4, 2
-        north[47]: J4[3 - 1],   # J4, 3
-        north[48]: J4[4 - 1],   # J4, 4
-        north[49]: J4[5 - 1],   # J4, 5
-        north[50]: J4[6 - 1],   # J4, 6
-        north[51]: J4[7 - 1],   # J4, 7
-        north[52]: J4[8 - 1],   # J4, 8
-        north[53]: J4[9 - 1],   # J4, 9
-        north[54]: J4[10 - 1],  # J4, 10
-        north[55]: J4[11 - 1],  # J4, 11
-        north[56]: J4[12 - 1],  # J4, 12
-        north[57]: J4[13 - 1],  # J4, 13
-        north[58]: J4[14 - 1],  # J4, 14
-        north[59]: J4[15 - 1],  # J4, 15
-        north[60]: J4[16 - 1],  # J4, 16
-        north[61]: J4[17 - 1],  # J4, 17
-        north[62]: J4[18 - 1],  # J4, 18
-        north[63]: J4[19 - 1],  # J4, 19
+        north[28]: J3[13 - 1],  # J3, 13
+        north[29]: J3[12 - 1],  # J3, 12
+        north[30]: J3[11 - 1],  # J3, 11
+        north[31]: J3[10 - 1],  # J3, 10
+        north[32]: J3[9 - 1],  # J3, 9
+        north[33]: J3[8 - 1],  # J3, 8
+        north[34]: J3[7 - 1],  # J3, 7
+        north[35]: J3[6 - 1],  # J3, 6
+        north[36]: J3[5 - 1],  # J3, 5
+        north[37]: J3[4 - 1],  # J3, 4
+        north[38]: J3[3 - 1],  # J3, 3
+        north[39]: J3[2 - 1],  # J3, 2
+        north[40]: J3[1 - 1],  # J3, 1
+        north[41]: J4[20 - 1],  # J4, 20
+        north[42]: J4[19 - 1],  # J4, 19
+        north[43]: J4[18 - 1],  # J4, 18
+        north[44]: J4[17 - 1],  # J4, 17
+        north[45]: J4[16 - 1],  # J4, 16
+        north[46]: J4[15 - 1],  # J4, 15
+        north[47]: J4[14 - 1],  # J4, 14
+        north[48]: J4[13 - 1],  # J4, 13
+        north[49]: J4[12 - 1],  # J4, 12
+        north[50]: J4[11 - 1],  # J4, 11
+        north[51]: J4[10 - 1],  # J4, 10
+        north[52]: J4[9 - 1],  # J4, 9
+        north[53]: J4[8 - 1],  # J4, 8
+        north[54]: J4[7 - 1],  # J4, 7
+        north[55]: J4[6 - 1],  # J4, 6
+        north[56]: J4[5 - 1],  # J4, 5
+        north[57]: J4[4 - 1],  # J4, 4
+        north[58]: J4[3 - 1],  # J4, 3
+        north[59]: J4[2 - 1],  # J4, 2
+        north[60]: J4[1 - 1],  # J4, 1
+        north[61]: J6[4 - 1],  # J6, 4
+        north[62]: J6[3 - 1],  # J6, 3
+        north[63]: J6[2 - 1],  # J6, 2
+        north[63]: J6[1 - 1],  # J6, 1
     }
 
 @dataclass:
 class chip_pin_out:
     # South Connector map of Pin Number: On-chip Device
     eq_pin_map = {
+        1: 'GND',
         2: 'FA_A',
         3: 'FA_B',
         4: 'HTR_D1_B',
